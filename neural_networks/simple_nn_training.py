a8ceaca08430513158c538946fd68386d7908a83
ishwar6 <iisudrj11@gmail.com>
2025-03-29 14:31:21 +0530

    Train a simple neural network on the MNIST dataset

---
+++ neural_networks/simple_nn_training.py
@@ -0,0 +1,47 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+
+class SimpleNN(nn.Module):
+    """
+    A simple feedforward neural network for classification tasks.
+    """
+    def __init__(self):
+        super(SimpleNN, self).__init__()
+        self.fc1 = nn.Linear(28 * 28, 128)
+        self.fc2 = nn.Linear(128, 10)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        x = x.view(-1, 28 * 28)
+        x = self.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+def train_model():
+    """
+    Trains the SimpleNN model on the MNIST dataset.
+    """
+    transform = transforms.Compose([transforms.ToTensor()])
+    train_dataset = datasets.MNIST(root='./data', train=True, download=True, transform=transform)
+    train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
+    model = SimpleNN()
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    model.train()
+
+    for epoch in range(5):
+        total_loss = 0
+        for images, labels in train_loader:
+            optimizer.zero_grad()
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            total_loss += loss.item()
+        print(f'Epoch {epoch+1}, Loss: {total_loss/len(train_loader)}')
+
+if __name__ == '__main__':
+    train_model()
