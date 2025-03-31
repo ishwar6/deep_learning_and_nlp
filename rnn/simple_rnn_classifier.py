be26358016c180f62634c8e717b484352ffd17fc
ishwar6 <iisudrj11@gmail.com>
2025-03-31 08:32:20 +0530

    Create a simple RNN for sequence classification with mock data

--- rnn/simple_rnn_classifier.py
+++ rnn/simple_rnn_classifier.py
@@ -2,62 +2,53 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import numpy as np
-from sklearn.model_selection import train_test_split
 
-class RNNModel(nn.Module):
-    """A simple RNN model for sequence classification."""
+class SimpleRNN(nn.Module):
+    """
+    A simple RNN model for sequence classification.
+    """
     def __init__(self, input_size, hidden_size, output_size):
-        super(RNNModel, self).__init__()
+        super(SimpleRNN, self).__init__()
         self.rnn = nn.RNN(input_size, hidden_size, batch_first=True)
         self.fc = nn.Linear(hidden_size, output_size)
 
     def forward(self, x):
-        """Forward pass through the RNN model."""
+        """
+        Forward pass through the RNN.
+        """
         out, _ = self.rnn(x)
         out = self.fc(out[:, -1, :])
         return out
 
 def generate_mock_data(seq_length, num_samples, input_size):
-    """Generates random data for mock training and testing."""
-    X = np.random.rand(num_samples, seq_length, input_size).astype(np.float32)
-    y = np.random.randint(0, 2, size=(num_samples,)).astype(np.long)
-    return X, y
+    """
+    Generates mock data for training.
+    """  
+    data = np.random.rand(num_samples, seq_length, input_size).astype(np.float32)
+    labels = np.random.randint(0, 2, size=(num_samples,))
+    return torch.from_numpy(data), torch.from_numpy(labels)
 
-def train_model(model, data_loader, criterion, optimizer, num_epochs):
-    """Trains the RNN model on the provided data loader."""
-    model.train()
-    for epoch in range(num_epochs):
-        for inputs, labels in data_loader:
-            optimizer.zero_grad()
-            outputs = model(inputs)
-            loss = criterion(outputs, labels)
-            loss.backward()
-            optimizer.step()
-    print(f'Training complete after {num_epochs} epochs.')
-
-def main():
-    """Main function to execute the training of the RNN model."""
-    input_size = 10
-    hidden_size = 20
-    output_size = 2
-    seq_length = 5
-    num_samples = 100
-    num_epochs = 10
-
-    X, y = generate_mock_data(seq_length, num_samples, input_size)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-    train_tensor = torch.tensor(X_train)
-    train_labels = torch.tensor(y_train)
-    test_tensor = torch.tensor(X_test)
-    test_labels = torch.tensor(y_test)
-
-    model = RNNModel(input_size, hidden_size, output_size)
+def train_rnn(model, data, labels, num_epochs=10, learning_rate=0.001):
+    """
+    Trains the RNN model with the given data and labels.
+    """  
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters())
-    train_loader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(train_tensor, train_labels), batch_size=16, shuffle=True)
-
-    train_model(model, train_loader, criterion, optimizer, num_epochs)
-    print(model)
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+    for epoch in range(num_epochs):
+        model.train()
+        optimizer.zero_grad()
+        outputs = model(data)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}')
 
 if __name__ == '__main__':
-    main()
+    input_size = 5
+    hidden_size = 10
+    output_size = 2
+    seq_length = 7
+    num_samples = 100
+    data, labels = generate_mock_data(seq_length, num_samples, input_size)
+    model = SimpleRNN(input_size, hidden_size, output_size)
+    train_rnn(model, data, labels)
