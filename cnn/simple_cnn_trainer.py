87dcb04b62c0d153c9fda3ee37170e95e3ffd645
ishwar6 <iisudrj11@gmail.com>
2025-03-31 12:15:32 +0530

    Add a simple CNN model training on MNIST dataset

--- cnn/simple_cnn_trainer.py
+++ cnn/simple_cnn_trainer.py
@@ -1,52 +1,61 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import torchvision.transforms as transforms
 import torchvision.datasets as datasets
+import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
 
 class SimpleCNN(nn.Module):
     """
-    A simple convolutional neural network for image classification.
+    A simple Convolutional Neural Network model for image classification.
     """
     def __init__(self):
         super(SimpleCNN, self).__init__()
-        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
         self.fc1 = nn.Linear(64 * 7 * 7, 128)
         self.fc2 = nn.Linear(128, 10)
         self.pool = nn.MaxPool2d(2, 2)
-        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(0.25)
 
     def forward(self, x):
-        x = self.pool(self.relu(self.conv1(x)))
-        x = self.pool(self.relu(self.conv2(x)))
+        x = self.pool(nn.functional.relu(self.conv1(x)))
+        x = self.pool(nn.functional.relu(self.conv2(x)))
         x = x.view(-1, 64 * 7 * 7)
-        x = self.relu(self.fc1(x))
+        x = self.dropout(nn.functional.relu(self.fc1(x)))
         x = self.fc2(x)
         return x
 
-def train_model():
+def train_model(model, train_loader, criterion, optimizer, epochs=5):
     """
-    Trains the SimpleCNN model on the MNIST dataset.
+    Trains the CNN model on the provided training data.
     """
-    transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
-    trainset = datasets.MNIST(root='./data', train=True, download=True, transform=transform)
-    trainloader = DataLoader(trainset, batch_size=64, shuffle=True)
-    model = SimpleCNN()
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(model.parameters(), lr=0.01)
-
-    for epoch in range(5):
+    model.train()
+    for epoch in range(epochs):
         running_loss = 0.0
-        for images, labels in trainloader:
+        for images, labels in train_loader:
             optimizer.zero_grad()
             outputs = model(images)
             loss = criterion(outputs, labels)
             loss.backward()
             optimizer.step()
             running_loss += loss.item()
-        print(f'Epoch {epoch + 1}, Loss: {running_loss / len(trainloader)}')
+        print(f'Epoch [{epoch + 1}/{epochs}], Loss: {running_loss / len(train_loader):.4f}')
+
+def main():
+    """
+    Main function to load data, initialize model, and train it.
+    """
+    transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
+    train_dataset = datasets.MNIST(root='./data', train=True, download=True, transform=transform)
+    train_loader = DataLoader(dataset=train_dataset, batch_size=64, shuffle=True)
+
+    model = SimpleCNN()
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+    train_model(model, train_loader, criterion, optimizer, epochs=5)
+    print('Training complete.')
 
 if __name__ == '__main__':
-    train_model()
+    main()
