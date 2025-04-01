2839663c102eccaf7334a5656ca091f07cd00cdd
ishwar6 <iisudrj11@gmail.com>
2025-04-01 10:02:22 +0530

    Add training script for a simple CNN on MNIST dataset

--- cnn/simple_cnn_trainer.py
+++ cnn/simple_cnn_trainer.py
@@ -1,13 +1,12 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import torchvision.datasets as datasets
-import torchvision.transforms as transforms
+from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 
 class SimpleCNN(nn.Module):
     """
-    A simple Convolutional Neural Network model for image classification.
+    A simple Convolutional Neural Network for image classification.
     """
     def __init__(self):
         super(SimpleCNN, self).__init__()
@@ -16,46 +15,43 @@ class SimpleCNN(nn.Module):
         self.fc1 = nn.Linear(64 * 7 * 7, 128)
         self.fc2 = nn.Linear(128, 10)
         self.pool = nn.MaxPool2d(2, 2)
-        self.dropout = nn.Dropout(0.25)
+        self.relu = nn.ReLU()
 
     def forward(self, x):
-        x = self.pool(nn.functional.relu(self.conv1(x)))
-        x = self.pool(nn.functional.relu(self.conv2(x)))
+        x = self.pool(self.relu(self.conv1(x)))
+        x = self.pool(self.relu(self.conv2(x)))
         x = x.view(-1, 64 * 7 * 7)
-        x = self.dropout(nn.functional.relu(self.fc1(x)))
+        x = self.relu(self.fc1(x))
         x = self.fc2(x)
         return x
 
-def train_model(model, train_loader, criterion, optimizer, epochs=5):
+def train(model, dataloader, criterion, optimizer, epochs=5):
     """
-    Trains the CNN model on the provided training data.
+    Train the CNN model.
     """
     model.train()
     for epoch in range(epochs):
         running_loss = 0.0
-        for images, labels in train_loader:
+        for images, labels in dataloader:
             optimizer.zero_grad()
             outputs = model(images)
             loss = criterion(outputs, labels)
             loss.backward()
             optimizer.step()
             running_loss += loss.item()
-        print(f'Epoch [{epoch + 1}/{epochs}], Loss: {running_loss / len(train_loader):.4f}')
+        print(f'Epoch {epoch + 1}, Loss: {running_loss / len(dataloader):.4f}')
 
 def main():
     """
-    Main function to load data, initialize model, and train it.
+    Main function to execute training of SimpleCNN on MNIST dataset.
     """
-    transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
-    train_dataset = datasets.MNIST(root='./data', train=True, download=True, transform=transform)
-    train_loader = DataLoader(dataset=train_dataset, batch_size=64, shuffle=True)
-
+    transform = transforms.Compose([transforms.ToTensor()])
+    train_dataset = datasets.MNIST(root='./data', train=True, transform=transform, download=True)
+    train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
     model = SimpleCNN()
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=0.001)
-
-    train_model(model, train_loader, criterion, optimizer, epochs=5)
-    print('Training complete.')
+    train(model, train_loader, criterion, optimizer)
 
 if __name__ == '__main__':
     main()
