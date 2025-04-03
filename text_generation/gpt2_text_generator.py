4724ea74366a3eb1d2b4f4b4d09ebaf83ea82a89
ishwar6 <iisudrj11@gmail.com>
2025-04-03 19:10:50 +0530

    Creates a text generation utility using GPT-2 model

--- text_generation/gpt2_text_generator.py
+++ text_generation/gpt2_text_generator.py
@@ -1,24 +1,24 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from transformers import GPT2LMHeadModel, GPT2Tokenizer
+from transformers import GPT2Tokenizer, GPT2LMHeadModel
 
 class TextGenerator:
     def __init__(self, model_name='gpt2'):
-        """Initializes the TextGenerator with a specified model."""
+        """Initializes the TextGenerator with a pre-trained model and tokenizer."""
         self.tokenizer = GPT2Tokenizer.from_pretrained(model_name)
         self.model = GPT2LMHeadModel.from_pretrained(model_name)
         self.model.eval()
 
     def generate_text(self, prompt, max_length=50):
-        """Generates text using the model given a prompt."""
-        inputs = self.tokenizer.encode(prompt, return_tensors='pt')
+        """Generates text given a prompt until the maximum length is reached."""
+        input_ids = self.tokenizer.encode(prompt, return_tensors='pt')
         with torch.no_grad():
-            outputs = self.model.generate(inputs, max_length=max_length, num_return_sequences=1)
-        return self.tokenizer.decode(outputs[0], skip_special_tokens=True)
+            output = self.model.generate(input_ids, max_length=max_length, num_return_sequences=1)
+        return self.tokenizer.decode(output[0], skip_special_tokens=True)
 
 if __name__ == '__main__':
     text_generator = TextGenerator()
-    prompt = 'In a future where technology has evolved,'
+    prompt = 'Once upon a time'
     generated_text = text_generator.generate_text(prompt)
     print(generated_text)
