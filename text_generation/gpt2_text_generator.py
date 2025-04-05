3202fb5f8d81a5ed67879b73f4ea08065a9eda76
ishwar6 <iisudrj11@gmail.com>
2025-04-06 05:24:07 +0530

    Create a text generator using GPT-2 model

--- text_generation/gpt2_text_generator.py
+++ text_generation/gpt2_text_generator.py
@@ -5,20 +5,20 @@ from transformers import GPT2Tokenizer, GPT2LMHeadModel
 
 class TextGenerator:
     def __init__(self, model_name='gpt2'):
-        """Initializes the TextGenerator with a pre-trained model and tokenizer."""
+        '''Initializes the text generator with a pre-trained model and tokenizer.'''
         self.tokenizer = GPT2Tokenizer.from_pretrained(model_name)
         self.model = GPT2LMHeadModel.from_pretrained(model_name)
         self.model.eval()
 
     def generate_text(self, prompt, max_length=50):
-        """Generates text given a prompt until the maximum length is reached."""
+        '''Generates text based on the provided prompt.'''
         input_ids = self.tokenizer.encode(prompt, return_tensors='pt')
         with torch.no_grad():
             output = self.model.generate(input_ids, max_length=max_length, num_return_sequences=1)
         return self.tokenizer.decode(output[0], skip_special_tokens=True)
 
 if __name__ == '__main__':
-    text_generator = TextGenerator()
-    prompt = 'Once upon a time'
-    generated_text = text_generator.generate_text(prompt)
-    print(generated_text)
+    generator = TextGenerator()
+    prompt = 'In a world where AI and humans coexist,'  
+    generated = generator.generate_text(prompt)
+    print(generated)
