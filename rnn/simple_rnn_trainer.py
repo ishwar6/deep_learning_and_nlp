9c65a23ef8c77c033122bccdab91a8bab924c769
ishwar6 <iisudrj11@gmail.com>
2025-04-05 16:32:02 +0530

    Implemented a simple RNN model and training routine

--- rnn/simple_rnn_trainer.py
+++ rnn/simple_rnn_trainer.py
@@ -2,61 +2,66 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader, Dataset
-from sklearn.model_selection import train_test_split
+import numpy as np
 
-class SimpleRNN(nn.Module):
+class SimpleDataset(Dataset):
+    """
+    A simple dataset for RNN training.
+    """
+    def __init__(self, sequences, labels):
+        self.sequences = sequences
+        self.labels = labels
+
+    def __len__(self):
+        return len(self.sequences)
+
+    def __getitem__(self, idx):
+        return self.sequences[idx], self.labels[idx]
+
+class RNNModel(nn.Module):
+    """
+    A simple RNN model with an embedding layer and a linear output layer.
+    """
     def __init__(self, input_size, hidden_size, output_size):
-        super(SimpleRNN, self).__init__()
-        self.rnn = nn.RNN(input_size, hidden_size, batch_first=True)
+        super(RNNModel, self).__init__()
+        self.embedding = nn.Embedding(input_size, hidden_size)
+        self.rnn = nn.RNN(hidden_size, hidden_size, batch_first=True)
         self.fc = nn.Linear(hidden_size, output_size)
 
     def forward(self, x):
+        x = self.embedding(x)
         out, _ = self.rnn(x)
         out = self.fc(out[:, -1, :])
         return out
 
-class SampleDataset(Dataset):
-    def __init__(self, data, labels):
-        self.data = data
-        self.labels = labels
-
-    def __len__(self):
-        return len(self.labels)
-
-    def __getitem__(self, idx):
-        return self.data[idx], self.labels[idx]
-
-def train_rnn_model():
-    input_size = 10
-    hidden_size = 20
-    output_size = 1
-    num_samples = 100
-    sequence_length = 5
-
-    data = torch.randn(num_samples, sequence_length, input_size)
-    labels = torch.randn(num_samples, output_size)
-
-    train_data, test_data, train_labels, test_labels = train_test_split(data, labels, test_size=0.2)
-    train_dataset = SampleDataset(train_data, train_labels)
-    test_dataset = SampleDataset(test_data, test_labels)
-
-    train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True)
-
-    model = SimpleRNN(input_size, hidden_size, output_size)
-    criterion = nn.MSELoss()
-    optimizer = optim.Adam(model.parameters(), lr=0.001)
-
-    for epoch in range(10):
-        for inputs, target in train_loader:
+def train_model(model, dataloader, criterion, optimizer, num_epochs):
+    """
+    Train the RNN model.
+    """
+    model.train()
+    for epoch in range(num_epochs):
+        for sequences, labels in dataloader:
             optimizer.zero_grad()
-            outputs = model(inputs)
-            loss = criterion(outputs, target)
+            outputs = model(sequences)
+            loss = criterion(outputs, labels)
             loss.backward()
             optimizer.step()
-        print(f'Epoch [{epoch+1}/10], Loss: {loss.item():.4f}')
-
-    print('Training complete')
-    return model
+        print(f'Epoch [{epoch + 1}/{num_epochs}], Loss: {loss.item():.4f}')
 
 if __name__ == '__main__':
-    trained_model = train_rnn_model()
+    np.random.seed(42)
+    torch.manual_seed(42)
+    input_size = 10
+    hidden_size = 20
+    output_size = 2
+    num_samples = 100
+    sequence_length = 5
+    sequences = torch.randint(0, input_size, (num_samples, sequence_length))
+    labels = torch.randint(0, output_size, (num_samples,))
+    dataset = SimpleDataset(sequences, labels)
+    dataloader = DataLoader(dataset, batch_size=16, shuffle=True)
+    model = RNNModel(input_size, hidden_size, output_size)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    train_model(model, dataloader, criterion, optimizer, num_epochs=5)
+    print('Training complete.')
