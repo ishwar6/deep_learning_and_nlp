5197a3121ba9acd5207db852f4a04c8bee5d1939
ishwar6 <iisudrj11@gmail.com>
2025-04-05 10:03:20 +0530

    Create a text generator using GPT-2 model for text completion

---
+++ openai_and_llms/gpt2_text_generator.py
@@ -0,0 +1,21 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from transformers import GPT2Tokenizer, GPT2LMHeadModel
+
+class TextGenerator:
+    def __init__(self, model_name='gpt2'):
+        self.tokenizer = GPT2Tokenizer.from_pretrained(model_name)
+        self.model = GPT2LMHeadModel.from_pretrained(model_name)
+        self.model.eval()
+
+    def generate_text(self, prompt, max_length=50):
+        inputs = self.tokenizer.encode(prompt, return_tensors='pt')
+        outputs = self.model.generate(inputs, max_length=max_length, num_return_sequences=1)
+        return self.tokenizer.decode(outputs[0], skip_special_tokens=True)
+
+if __name__ == '__main__':
+    text_generator = TextGenerator()
+    prompt = 'In the future, AI will'
+    generated_text = text_generator.generate_text(prompt)
+    print(generated_text)
