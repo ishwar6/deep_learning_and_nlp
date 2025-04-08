08cc5d164eb884d5113bbb3e3b2019ef97daf863
ishwar6 <iisudrj11@gmail.com>
2025-04-08 14:08:48 +0530

    Create a simple RNN model for sequence classification and training

--- rnn/simple_rnn_classifier.py
+++ rnn/simple_rnn_classifier.py
@@ -2,10 +2,11 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader, Dataset
-import numpy as np
 
 class SimpleRNN(nn.Module):
-    """A simple RNN model for sequence classification."""
+    """
+    A simple RNN model for sequence classification.
+    """
     def __init__(self, input_size, hidden_size, output_size):
         super(SimpleRNN, self).__init__()
         self.rnn = nn.RNN(input_size, hidden_size, batch_first=True)
@@ -16,38 +17,46 @@ class SimpleRNN(nn.Module):
         out = self.fc(rnn_out[:, -1, :])
         return out
 
-class RandomDataset(Dataset):
-    """Generates random data for training the RNN."""
-    def __init__(self, num_samples, seq_length, input_size):
-        self.data = torch.randn(num_samples, seq_length, input_size)
-        self.labels = torch.randint(0, 2, (num_samples,))
+class SampleDataset(Dataset):
+    """
+    A simple dataset class for generating mock data.
+    """
+    def __init__(self, num_samples, seq_length, input_size, output_size):
+        self.data = torch.rand(num_samples, seq_length, input_size)
+        self.labels = torch.randint(0, output_size, (num_samples,))
 
     def __len__(self):
-        return len(self.labels)
+        return len(self.data)
 
     def __getitem__(self, idx):
         return self.data[idx], self.labels[idx]
 
-def train_model(model, dataloader, epochs=5):
-    """Trains the RNN model on the provided data loader."""
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters(), lr=0.001)
-    for epoch in range(epochs):
+def train_model(model, dataloader, criterion, optimizer, num_epochs):
+    """
+    Train the RNN model on the dataset.
+    """
+    for epoch in range(num_epochs):
         for inputs, labels in dataloader:
             optimizer.zero_grad()
             outputs = model(inputs)
             loss = criterion(outputs, labels)
             loss.backward()
             optimizer.step()
-        print(f'Epoch {epoch + 1}/{epochs}, Loss: {loss.item():.4f}')
+        print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}')
 
 if __name__ == '__main__':
     input_size = 10
     hidden_size = 20
-    output_size = 2
-    seq_length = 5
+    output_size = 5
     num_samples = 100
-    dataset = RandomDataset(num_samples, seq_length, input_size)
+    seq_length = 15
+    num_epochs = 5
+
+    dataset = SampleDataset(num_samples, seq_length, input_size, output_size)
     dataloader = DataLoader(dataset, batch_size=16, shuffle=True)
+
     model = SimpleRNN(input_size, hidden_size, output_size)
-    train_model(model, dataloader, epochs=3)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+    train_model(model, dataloader, criterion, optimizer, num_epochs)
