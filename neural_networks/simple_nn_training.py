a1ebea060f3624f8763c4b3316664ef72fae78cc
ishwar6 <iisudrj11@gmail.com>
2025-04-08 13:30:51 +0530

    Add a simple neural network training example with random data

--- neural_networks/simple_nn_training.py
+++ neural_networks/simple_nn_training.py
@@ -1,47 +1,62 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torchvision import datasets, transforms
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Dataset
+import numpy as np
+
+class SimpleDataset(Dataset):
+    """
+    A simple dataset class that generates random data.
+    """
+    def __init__(self, size):
+        self.size = size
+        self.data = np.random.rand(size, 10).astype(np.float32)
+        self.labels = (self.data.sum(axis=1) > 5).astype(np.float32)
+
+    def __len__(self):
+        return self.size
+
+    def __getitem__(self, idx):
+        return torch.tensor(self.data[idx]), torch.tensor(self.labels[idx])
 
 class SimpleNN(nn.Module):
     """
-    A simple feedforward neural network for classification tasks.
+    A simple feedforward neural network model.
     """
     def __init__(self):
         super(SimpleNN, self).__init__()
-        self.fc1 = nn.Linear(28 * 28, 128)
-        self.fc2 = nn.Linear(128, 10)
-        self.relu = nn.ReLU()
+        self.fc1 = nn.Linear(10, 5)
+        self.fc2 = nn.Linear(5, 1)
 
     def forward(self, x):
-        x = x.view(-1, 28 * 28)
-        x = self.relu(self.fc1(x))
-        x = self.fc2(x)
+        x = torch.relu(self.fc1(x))
+        x = torch.sigmoid(self.fc2(x))
         return x
 
-def train_model():
+def train_model(model, data_loader, criterion, optimizer, epochs):
     """
-    Trains the SimpleNN model on the MNIST dataset.
+    Train the neural network model.
     """
-    transform = transforms.Compose([transforms.ToTensor()])
-    train_dataset = datasets.MNIST(root='./data', train=True, download=True, transform=transform)
-    train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
-    model = SimpleNN()
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters(), lr=0.001)
     model.train()
-
-    for epoch in range(5):
-        total_loss = 0
-        for images, labels in train_loader:
+    for epoch in range(epochs):
+        for inputs, labels in data_loader:
             optimizer.zero_grad()
-            outputs = model(images)
-            loss = criterion(outputs, labels)
+            outputs = model(inputs)
+            loss = criterion(outputs.squeeze(), labels)
             loss.backward()
             optimizer.step()
-            total_loss += loss.item()
-        print(f'Epoch {epoch+1}, Loss: {total_loss/len(train_loader)}')
+        print(f'Epoch {epoch+1}/{epochs}, Loss: {loss.item():.4f}')
+
+def main():
+    """
+    Main function to set up the dataset, model, and initiate training.
+    """
+    dataset = SimpleDataset(size=1000)
+    data_loader = DataLoader(dataset, batch_size=32, shuffle=True)
+    model = SimpleNN()
+    criterion = nn.BCELoss()
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    train_model(model, data_loader, criterion, optimizer, epochs=10)
 
 if __name__ == '__main__':
-    train_model()
+    main()
