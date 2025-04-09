c5ece90e8094fd048c908de66ac4fa6da64ec4f9
ishwar6 <iisudrj11@gmail.com>
2025-04-09 11:13:58 +0530

    Add BART-based text summarization functionality

--- summarization/text_summarizer.py
+++ summarization/text_summarizer.py
@@ -1,21 +1,19 @@
 import torch
+import torch.nn as nn
 from transformers import BartTokenizer, BartForConditionalGeneration
 
-class Summarizer:
+class TextSummarizer:
     def __init__(self):
-        """Initializes the model and tokenizer for summarization."""
-        self.tokenizer = BartTokenizer.from_pretrained('facebook/bart-large')
-        self.model = BartForConditionalGeneration.from_pretrained('facebook/bart-large')
+        self.tokenizer = BartTokenizer.from_pretrained('facebook/bart-large-cnn')
+        self.model = BartForConditionalGeneration.from_pretrained('facebook/bart-large-cnn')
 
     def summarize(self, text):
-        """Generates a summary for the given text."""
         inputs = self.tokenizer.encode(text, return_tensors='pt', max_length=1024, truncation=True)
         summary_ids = self.model.generate(inputs, max_length=150, min_length=40, length_penalty=2.0, num_beams=4, early_stopping=True)
         summary = self.tokenizer.decode(summary_ids[0], skip_special_tokens=True)
         return summary
 
 if __name__ == '__main__':
-    text = "Deep learning is a subset of machine learning that deals with neural networks. It has gained immense popularity due to its ability to analyze large amounts of data and produce powerful models for tasks like image recognition and natural language processing."
-    summarizer = Summarizer()
-    summary = summarizer.summarize(text)
-    print('Summary:', summary)
+    text = "Deep learning is a subset of machine learning in artificial intelligence that deals with algorithms inspired by the structure and function of the brain. It is used in various applications including image recognition, natural language processing, and more."
+    summarizer = TextSummarizer()
+    print(summarizer.summarize(text))
