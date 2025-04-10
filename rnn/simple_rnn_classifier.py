8800d1b6da5dc3a4a3020349aefaf0cd5cd86683
ishwar6 <iisudrj11@gmail.com>
2025-04-10 11:35:15 +0530

    Create a simple RNN for classifying FashionMNIST dataset

--- rnn/simple_rnn_classifier.py
+++ rnn/simple_rnn_classifier.py
@@ -1,12 +1,11 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torch.utils.data import DataLoader, Dataset
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
 
 class SimpleRNN(nn.Module):
-    """
-    A simple RNN model for sequence classification.
-    """
+    """A simple RNN model for sequence classification."""
     def __init__(self, input_size, hidden_size, output_size):
         super(SimpleRNN, self).__init__()
         self.rnn = nn.RNN(input_size, hidden_size, batch_first=True)
@@ -17,46 +16,35 @@ class SimpleRNN(nn.Module):
         out = self.fc(rnn_out[:, -1, :])
         return out
 
-class SampleDataset(Dataset):
-    """
-    A simple dataset class for generating mock data.
-    """
-    def __init__(self, num_samples, seq_length, input_size, output_size):
-        self.data = torch.rand(num_samples, seq_length, input_size)
-        self.labels = torch.randint(0, output_size, (num_samples,))
-
-    def __len__(self):
-        return len(self.data)
-
-    def __getitem__(self, idx):
-        return self.data[idx], self.labels[idx]
-
-def train_model(model, dataloader, criterion, optimizer, num_epochs):
-    """
-    Train the RNN model on the dataset.
-    """
-    for epoch in range(num_epochs):
-        for inputs, labels in dataloader:
+def train_rnn_model(model, train_loader, criterion, optimizer, epochs=5):
+    """Trains the RNN model on the provided data loader."""
+    model.train()
+    for epoch in range(epochs):
+        for inputs, labels in train_loader:
             optimizer.zero_grad()
             outputs = model(inputs)
             loss = criterion(outputs, labels)
             loss.backward()
             optimizer.step()
-        print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}')
+        print(f'Epoch [{epoch+1}/{epochs}], Loss: {loss.item():.4f}')
 
-if __name__ == '__main__':
-    input_size = 10
-    hidden_size = 20
-    output_size = 5
-    num_samples = 100
-    seq_length = 15
-    num_epochs = 5
+def main():
+    """Main function to set up data and run training."""
+    input_size = 28
+    hidden_size = 64
+    output_size = 10
+    batch_size = 32
+    epochs = 5
 
-    dataset = SampleDataset(num_samples, seq_length, input_size, output_size)
-    dataloader = DataLoader(dataset, batch_size=16, shuffle=True)
+    transform = transforms.Compose([transforms.ToTensor()])
+    train_dataset = datasets.FashionMNIST(root='./data', train=True, download=True, transform=transform)
+    train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
 
     model = SimpleRNN(input_size, hidden_size, output_size)
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=0.001)
 
-    train_model(model, dataloader, criterion, optimizer, num_epochs)
+    train_rnn_model(model, train_loader, criterion, optimizer, epochs)
+
+if __name__ == '__main__':
+    main()
