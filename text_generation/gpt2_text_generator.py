59f381cd70dc9908c7048b38c56bf700149d30e4
ishwar6 <iisudrj11@gmail.com>
2025-04-10 14:36:44 +0530

    Create a text generator using pre-trained GPT-2 model

--- text_generation/gpt2_text_generator.py
+++ text_generation/gpt2_text_generator.py
@@ -1,34 +1,24 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from transformers import GPT2LMHeadModel, GPT2Tokenizer
+from transformers import GPT2Tokenizer, GPT2LMHeadModel
 
 class TextGenerator:
     def __init__(self, model_name='gpt2'):
+        """Initializes the text generator with a pre-trained GPT-2 model."""
         self.tokenizer = GPT2Tokenizer.from_pretrained(model_name)
         self.model = GPT2LMHeadModel.from_pretrained(model_name)
+        self.model.eval()
 
     def generate_text(self, prompt, max_length=50):
+        """Generates text based on a given prompt."""
         inputs = self.tokenizer.encode(prompt, return_tensors='pt')
-        outputs = self.model.generate(inputs, max_length=max_length, num_return_sequences=1)
+        with torch.no_grad():
+            outputs = self.model.generate(inputs, max_length=max_length, num_return_sequences=1)
         return self.tokenizer.decode(outputs[0], skip_special_tokens=True)
-
-    def fine_tune(self, train_data, epochs=1):
-        self.model.train()
-        optimizer = optim.Adam(self.model.parameters(), lr=5e-5)
-        for epoch in range(epochs):
-            for text in train_data:
-                inputs = self.tokenizer.encode(text, return_tensors='pt')
-                labels = inputs.clone()
-                outputs = self.model(inputs, labels=labels)
-                loss = outputs.loss
-                optimizer.zero_grad()
-                loss.backward()
-                optimizer.step()
-                print(f'Epoch: {epoch + 1}, Loss: {loss.item()}')
 
 if __name__ == '__main__':
     generator = TextGenerator()
-    print(generator.generate_text('Once upon a time', max_length=30))
-    mock_train_data = ['The quick brown fox jumps over the lazy dog.', 'Deep learning is fascinating.']
-    generator.fine_tune(mock_train_data, epochs=2)
+    prompt = 'In a distant future,'
+    generated_text = generator.generate_text(prompt)
+    print(generated_text)
