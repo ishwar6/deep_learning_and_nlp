170b903a971f932b38c26b5b1ca62ed7b37bc8f4
ishwar6 <iisudrj11@gmail.com>
2025-04-19 06:52:07 +0530

    Developed a simple CNN for MNIST image classification

---
+++ cnn/simple_cnn_training.py
@@ -0,0 +1,59 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+
+class SimpleCNN(nn.Module):
+    """
+    A simple Convolutional Neural Network for image classification.
+    """
+    def __init__(self):
+        super(SimpleCNN, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        self.fc1 = nn.Linear(64 * 7 * 7, 128)
+        self.fc2 = nn.Linear(128, 10)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        x = self.pool(self.relu(self.conv1(x)))
+        x = self.pool(self.relu(self.conv2(x)))
+        x = x.view(-1, 64 * 7 * 7)
+        x = self.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+def train_model(model, train_loader, criterion, optimizer, epochs=5):
+    """
+    Train the CNN model with the provided training data.
+    """
+    model.train()
+    for epoch in range(epochs):
+        running_loss = 0.0
+        for images, labels in train_loader:
+            optimizer.zero_grad()
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item()
+        print(f'Epoch [{epoch + 1}/{epochs}], Loss: {running_loss / len(train_loader):.4f}')
+
+def main():
+    """
+    Main function to execute the training pipeline.
+    """
+    transform = transforms.Compose([transforms.Resize((28, 28)), transforms.ToTensor()])
+    train_dataset = datasets.MNIST(root='./data', train=True, download=True, transform=transform)
+    train_loader = DataLoader(dataset=train_dataset, batch_size=64, shuffle=True)
+
+    model = SimpleCNN()
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+    train_model(model, train_loader, criterion, optimizer)
+
+if __name__ == '__main__':
+    main()
