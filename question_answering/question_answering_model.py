a958874193876b9adc27722fceeda7522fb89896
ishwar6 <iisudrj11@gmail.com>
2025-07-22 22:25:08 +0530

    Developed a BERT-based question answering model with training loop

--- question_answering/question_answering_model.py
+++ question_answering/question_answering_model.py
@@ -2,47 +2,45 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from transformers import BertTokenizer, BertForQuestionAnswering
-from transformers import Trainer, TrainingArguments
+from torch.utils.data import DataLoader, Dataset
 
-class QAData:
+class QADataset(Dataset):
     def __init__(self, questions, contexts, answers):
         self.questions = questions
         self.contexts = contexts
         self.answers = answers
+        self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 
-    def tokenize(self):
-        tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-        inputs = tokenizer(self.questions, self.contexts, return_tensors='pt', padding=True, truncation=True)
-        return inputs
+    def __len__(self):
+        return len(self.questions)
+
+    def __getitem__(self, idx):
+        encoding = self.tokenizer(self.questions[idx], self.contexts[idx], return_tensors='pt', truncation=True, padding='max_length', max_length=512)
+        start_position = self.answers[idx]['start']
+        end_position = self.answers[idx]['end']
+        return {**encoding, 'start_positions': torch.tensor(start_position), 'end_positions': torch.tensor(end_position)}
 
 class QAModel:
     def __init__(self):
         self.model = BertForQuestionAnswering.from_pretrained('bert-base-uncased')
+        self.optimizer = optim.Adam(self.model.parameters(), lr=5e-5)
 
-    def train(self, inputs, answers):
-        optimizer = optim.AdamW(self.model.parameters(), lr=5e-5)
+    def train(self, dataloader, epochs=3):
         self.model.train()
-        for epoch in range(3):
-            optimizer.zero_grad()
-            outputs = self.model(**inputs)
-            loss = outputs.loss
-            loss.backward()
-            optimizer.step()
-            print(f'Epoch {epoch + 1}, Loss: {loss.item()}')
+        for epoch in range(epochs):
+            for batch in dataloader:
+                self.optimizer.zero_grad()
+                outputs = self.model(**batch)
+                loss = outputs.loss
+                loss.backward()
+                self.optimizer.step()
+                print(f'Epoch {epoch + 1}, Loss: {loss.item()}')
 
-    def predict(self, inputs):
-        self.model.eval()
-        with torch.no_grad():
-            outputs = self.model(**inputs)
-        return outputs.start_logits.argmax(), outputs.end_logits.argmax()
+questions = ['What is the capital of France?']
+contexts = ['The capital of France is Paris.']
+answers = [{'start': 30, 'end': 35}]
 
-if __name__ == '__main__':
-    questions = ['What is the capital of France?']
-    contexts = ['The capital of France is Paris.']
-    answers = ['Paris']
-    qa_data = QAData(questions, contexts, answers)
-    tokenized_inputs = qa_data.tokenize()
-    qa_model = QAModel()
-    qa_model.train(tokenized_inputs, answers)
-    start, end = qa_model.predict(tokenized_inputs)
-    print(f'Predicted answer spans from {start} to {end}')
+dataset = QADataset(questions, contexts, answers)
+dataloader = DataLoader(dataset, batch_size=1)
+model = QAModel()
+model.train(dataloader)
