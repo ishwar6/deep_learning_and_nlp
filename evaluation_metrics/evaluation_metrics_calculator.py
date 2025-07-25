66954a5400c4bd81d1ce9efe8e32815b03e1f119
ishwar6 <iisudrj11@gmail.com>
2025-07-25 22:06:44 +0530

    Calculates accuracy, precision, recall, and F1 score for predictions

--- evaluation_metrics/evaluation_metrics_calculator.py
+++ evaluation_metrics/evaluation_metrics_calculator.py
@@ -3,31 +3,47 @@ from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_sc
 
 class EvaluationMetrics:
     def __init__(self, y_true, y_pred):
-        """Initialize the EvaluationMetrics with true and predicted labels."""
-        self.y_true = y_true
-        self.y_pred = y_pred
+        """
+        Initializes the EvaluationMetrics with true and predicted labels.
+        :param y_true: List of true labels
+        :param y_pred: List of predicted labels
+        """
+        self.y_true = np.array(y_true)
+        self.y_pred = np.array(y_pred)
 
-    def calculate_accuracy(self):
-        """Calculate accuracy of the predictions."""
+    def accuracy(self):
+        """
+        Calculates the accuracy of predictions.
+        :return: Accuracy score
+        """
         return accuracy_score(self.y_true, self.y_pred)
 
-    def calculate_precision(self):
-        """Calculate precision of the predictions."""
+    def precision(self):
+        """
+        Calculates the precision of predictions.
+        :return: Precision score
+        """
         return precision_score(self.y_true, self.y_pred, average='weighted')
 
-    def calculate_recall(self):
-        """Calculate recall of the predictions."""
+    def recall(self):
+        """
+        Calculates the recall of predictions.
+        :return: Recall score
+        """
         return recall_score(self.y_true, self.y_pred, average='weighted')
 
-    def calculate_f1(self):
-        """Calculate F1 score of the predictions."""
+    def f1(self):
+        """
+        Calculates the F1 score of predictions.
+        :return: F1 score
+        """
         return f1_score(self.y_true, self.y_pred, average='weighted')
 
 if __name__ == '__main__':
-    y_true = np.array([1, 0, 1, 1, 0, 1])
-    y_pred = np.array([1, 0, 1, 0, 0, 1])
-    metrics = EvaluationMetrics(y_true, y_pred)
-    print(f'Accuracy: {metrics.calculate_accuracy()}')
-    print(f'Precision: {metrics.calculate_precision()}')
-    print(f'Recall: {metrics.calculate_recall()}')
-    print(f'F1 Score: {metrics.calculate_f1()}')
+    true_labels = [1, 0, 1, 1, 0, 1, 0, 0, 1, 0]
+    predicted_labels = [1, 0, 1, 0, 0, 1, 1, 0, 1, 0]
+    evaluator = EvaluationMetrics(true_labels, predicted_labels)
+    print('Accuracy:', evaluator.accuracy())
+    print('Precision:', evaluator.precision())
+    print('Recall:', evaluator.recall())
+    print('F1 Score:', evaluator.f1())
