baf0f2ea47632bb8b7e9f5b98fe9684afadea517
ishwar6 <iisudrj11@gmail.com>
2025-07-28 19:22:17 +0530

    Create a text generation utility using GPT-2 model

--- text_generation/gpt2_text_generator.py
+++ text_generation/gpt2_text_generator.py
@@ -7,15 +7,18 @@ class TextGenerator:
     def __init__(self, model_name='gpt2'):
         self.tokenizer = GPT2Tokenizer.from_pretrained(model_name)
         self.model = GPT2LMHeadModel.from_pretrained(model_name)
-        self.model.eval()
 
     def generate_text(self, prompt, max_length=50):
         inputs = self.tokenizer.encode(prompt, return_tensors='pt')
         outputs = self.model.generate(inputs, max_length=max_length, num_return_sequences=1)
-        return self.tokenizer.decode(outputs[0], skip_special_tokens=True)
+        generated_text = self.tokenizer.decode(outputs[0], skip_special_tokens=True)
+        return generated_text
 
-if __name__ == '__main__':
+def main():
     generator = TextGenerator()
     prompt = 'Once upon a time'
     generated_text = generator.generate_text(prompt)
-    print(generated_text)
+    print('Generated Text:\n', generated_text)
+
+if __name__ == '__main__':
+    main()
