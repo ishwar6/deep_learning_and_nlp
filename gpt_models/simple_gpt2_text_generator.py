db9a2108e5c4fa783743ee0d3fbb00e3c2fea88f
ishwar6 <iisudrj11@gmail.com>
2025-07-28 17:46:01 +0530

    Create a simple GPT-2 text generation class

--- gpt_models/simple_gpt2_text_generator.py
+++ gpt_models/simple_gpt2_text_generator.py
@@ -3,20 +3,17 @@ import torch.nn as nn
 import torch.optim as optim
 from transformers import GPT2Tokenizer, GPT2LMHeadModel
 
-class SimpleGPT2Model:
-    def __init__(self, model_name='gpt2'):
-        """Initializes the GPT-2 model and tokenizer."""
-        self.tokenizer = GPT2Tokenizer.from_pretrained(model_name)
-        self.model = GPT2LMHeadModel.from_pretrained(model_name)
+class SimpleGPT2:
+    def __init__(self):
+        self.tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+        self.model = GPT2LMHeadModel.from_pretrained('gpt2')
 
     def generate_text(self, prompt, max_length=50):
-        """Generates text based on a given prompt using the GPT-2 model."""
         inputs = self.tokenizer.encode(prompt, return_tensors='pt')
         outputs = self.model.generate(inputs, max_length=max_length, num_return_sequences=1)
         return self.tokenizer.decode(outputs[0], skip_special_tokens=True)
 
 if __name__ == '__main__':
-    gpt2_model = SimpleGPT2Model()
-    prompt = 'Once upon a time in a land far away'
-    generated_text = gpt2_model.generate_text(prompt)
+    gpt2_instance = SimpleGPT2()
+    generated_text = gpt2_instance.generate_text('Once upon a time')
     print(generated_text)
