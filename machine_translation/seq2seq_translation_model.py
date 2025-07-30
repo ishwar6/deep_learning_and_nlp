bfdddccd379b176b9a46a96b92aac9d2a706fd6c
ishwar6 <iisudrj11@gmail.com>
2025-07-30 19:44:28 +0530

    Create Seq2Seq model for German to English translation

--- machine_translation/seq2seq_translation_model.py
+++ machine_translation/seq2seq_translation_model.py
@@ -1,91 +1,78 @@
 import torch
 import torch.nn as nn
-import torch.optim as optim
 from torchtext.datasets import Multi30k
 from torchtext.data import Field, BucketIterator
-from transformers import BertTokenizer, BertModel
+import spacy
 
 class Seq2Seq(nn.Module):
     def __init__(self, encoder, decoder):
-        super().__init__()
+        super(Seq2Seq, self).__init__()
         self.encoder = encoder
         self.decoder = decoder
 
     def forward(self, src, trg, teacher_forcing_ratio=0.5):
-        trg_len = trg.shape[1]
-        batch_size = trg.shape[0]
-        vocab_size = self.decoder.output_dim
-        outputs = torch.zeros(batch_size, trg_len, vocab_size).to(trg.device)
+        src_len = src.shape[0]
+        batch_size = trg.shape[1]
+        trg_len = trg.shape[0]
+        outputs = torch.zeros(trg_len, batch_size, self.decoder.output_dim)
 
         encoder_outputs, hidden = self.encoder(src)
-        input = trg[:, 0]
+        input = trg[0, :]  
 
         for t in range(1, trg_len):
             output, hidden = self.decoder(input, hidden, encoder_outputs)
-            outputs[:, t] = output
+            outputs[t] = output
             teacher_force = torch.rand(1) < teacher_forcing_ratio
-            input = trg[:, t] if teacher_force else output.argmax(1)
+            input = trg[t] if teacher_force else output.argmax(1)
 
         return outputs
 
 class Encoder(nn.Module):
-    def __init__(self, input_dim, emb_dim, hidden_dim):
-        super().__init__()
+    def __init__(self, input_dim, emb_dim, hidden_dim, n_layers, dropout):
+        super(Encoder, self).__init__()
         self.embedding = nn.Embedding(input_dim, emb_dim)
-        self.rnn = nn.GRU(emb_dim, hidden_dim)
+        self.rnn = nn.GRU(emb_dim, hidden_dim, n_layers, dropout=dropout)
+        self.dropout = nn.Dropout(dropout)
 
     def forward(self, src):
-        embedded = self.embedding(src)
+        embedded = self.dropout(self.embedding(src))
         outputs, hidden = self.rnn(embedded)
         return outputs, hidden
 
 class Decoder(nn.Module):
-    def __init__(self, output_dim, emb_dim, hidden_dim):
-        super().__init__()
+    def __init__(self, output_dim, emb_dim, hidden_dim, n_layers, dropout):
+        super(Decoder, self).__init__()
+        self.output_dim = output_dim
         self.embedding = nn.Embedding(output_dim, emb_dim)
-        self.rnn = nn.GRU(emb_dim, hidden_dim)
+        self.rnn = nn.GRU(emb_dim, hidden_dim, n_layers, dropout=dropout)
         self.fc_out = nn.Linear(hidden_dim, output_dim)
+        self.dropout = nn.Dropout(dropout)
 
     def forward(self, input, hidden, encoder_outputs):
         input = input.unsqueeze(0)
-        embedded = self.embedding(input)
+        embedded = self.dropout(self.embedding(input))
         output, hidden = self.rnn(embedded, hidden)
-        prediction = self.fc_out(output)
-        return prediction.squeeze(0), hidden
+        prediction = self.fc_out(output.squeeze(0))
+        return prediction, hidden
 
 def main():
-    SRC = Field(tokenize='spacy', init_token='<sos>', eos_token='<eos>')
-    TRG = Field(tokenize='spacy', init_token='<sos>', eos_token='<eos>')
+    SRC = Field(tokenize='spacy', init_token='<sos>', eos_token='<eos>', lower=True)
+    TRG = Field(tokenize='spacy', init_token='<sos>', eos_token='<eos>', lower=True)
     train_data, valid_data, test_data = Multi30k.splits(exts=('.de', '.en'), fields=(SRC, TRG))
     SRC.build_vocab(train_data, min_freq=2)
     TRG.build_vocab(train_data, min_freq=2)
-
-    INPUT_DIM = len(SRC.vocab)
-    OUTPUT_DIM = len(TRG.vocab)
-    EMB_DIM = 256
-    HIDDEN_DIM = 512
-
-    encoder = Encoder(INPUT_DIM, EMB_DIM, HIDDEN_DIM)
-    decoder = Decoder(OUTPUT_DIM, EMB_DIM, HIDDEN_DIM)
+    input_dim = len(SRC.vocab)
+    output_dim = len(TRG.vocab)
+    enc_emb_dim = 256
+    dec_emb_dim = 256
+    enc_hidden_dim = 512
+    dec_hidden_dim = 512
+    n_layers = 2
+    dropout = 0.5
+    encoder = Encoder(input_dim, enc_emb_dim, enc_hidden_dim, n_layers, dropout)
+    decoder = Decoder(output_dim, dec_emb_dim, dec_hidden_dim, n_layers, dropout)
     model = Seq2Seq(encoder, decoder)
-
-    optimizer = optim.Adam(model.parameters())
-    criterion = nn.CrossEntropyLoss()
-
-    for epoch in range(10):
-        for i, batch in enumerate(BucketIterator(train_data, batch_size=32)): 
-            src = batch.src
-            trg = batch.trg
-            optimizer.zero_grad()
-            output = model(src, trg)
-            output_dim = output.shape[-1]
-            output = output[1:].view(-1, output_dim)
-            trg = trg[1:].view(-1)
-            loss = criterion(output, trg)
-            loss.backward()
-            optimizer.step()
-            if i % 100 == 0:
-                print(f'Epoch: {epoch}, Batch: {i}, Loss: {loss.item()}')
+    print(model)
 
 if __name__ == '__main__':
     main()
