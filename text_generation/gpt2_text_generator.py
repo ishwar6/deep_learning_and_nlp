5158ef871cb4fe659e756c32a7f84ee4c99ab99b
ishwar6 <iisudrj11@gmail.com>
2025-07-31 21:54:36 +0530

    Create a text generator using GPT-2 with training capability

--- text_generation/gpt2_text_generator.py
+++ text_generation/gpt2_text_generator.py
@@ -11,14 +11,29 @@ class TextGenerator:
     def generate_text(self, prompt, max_length=50):
         inputs = self.tokenizer.encode(prompt, return_tensors='pt')
         outputs = self.model.generate(inputs, max_length=max_length, num_return_sequences=1)
-        generated_text = self.tokenizer.decode(outputs[0], skip_special_tokens=True)
-        return generated_text
+        return self.tokenizer.decode(outputs[0], skip_special_tokens=True)
 
-def main():
-    generator = TextGenerator()
-    prompt = 'Once upon a time'
-    generated_text = generator.generate_text(prompt)
-    print('Generated Text:\n', generated_text)
+    def train(self, dataset, epochs=1, learning_rate=5e-5):
+        optimizer = optim.Adam(self.model.parameters(), lr=learning_rate)
+        self.model.train()
+        for epoch in range(epochs):
+            for text in dataset:
+                inputs = self.tokenizer.encode(text, return_tensors='pt')
+                labels = inputs.clone()
+                optimizer.zero_grad()
+                outputs = self.model(inputs, labels=labels)
+                loss = outputs.loss
+                loss.backward()
+                optimizer.step()
+                print(f'Epoch: {epoch + 1}, Loss: {loss.item()}')
 
 if __name__ == '__main__':
-    main()
+    sample_dataset = [
+        'Once upon a time in a land far away.',
+        'The quick brown fox jumps over the lazy dog.',
+        'To be or not to be, that is the question.'
+    ]
+    generator = TextGenerator()
+    generator.train(sample_dataset, epochs=2)
+    generated_text = generator.generate_text('In a world where technology', max_length=30)
+    print(generated_text)
